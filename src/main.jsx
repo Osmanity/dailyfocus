32293b986d15e285742cb0e41967ae3fb7d3722a
Ibrahim Osman <mr.ibrahim@hotmail.se>
2024-12-12 11:24:57 +0100

    Created all necessary Routes, styled Sidebar

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,27 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter, Route, Routes } from "react-router";
+import Sidebar from "./components/Sidebar.jsx";
+import Overview from "./pages/Overview.jsx";
+import Todos from "./pages/Todos/Todos.jsx";
+import Habits from "./pages/Habits/Habits.jsx";
+import Events from "./pages/Events/Events.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
+      <div className="layout">
+        <Sidebar />
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/overview" element={<Overview />} />
+            <Route path="/todos" element={<Todos />} />
+            <Route path="/habits" element={<Habits />} />
+            <Route path="/events" element={<Events />} />
+          </Routes>
+        </div>
+      </div>
     </BrowserRouter>
   </StrictMode>
 );
