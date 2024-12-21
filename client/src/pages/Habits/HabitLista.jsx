f1f27c7859036ba8ee1e085b262c4fde4b9a66e3
AishaHX <ayeshggz@gmail.com>
2024-12-21 21:53:06 +0100

    fixat error, och designat om den till globala designen

--- client/src/pages/Habits/HabitLista.jsx
+++ client/src/pages/Habits/HabitLista.jsx
@@ -2,42 +2,52 @@ import styles from "./Habit.module.css";
 
 function HabitLista({ rutiner, tarbort, updatera }) {
   return (
-    <div className={styles.rutinCard}>
-      <ul className={styles.listContainer}>
-        {rutiner.map((rutin) => (
-          <li key={rutin.id} className={styles.list}>
-            <div className={styles.habitlistText}>
-              <h2> {rutin.title} </h2> Prioritet är
-              <strong> {rutin.Prioritet} </strong>, upprepas{" "}
-              <strong>{rutin.Repetitioner}</strong> gånger.
+    <div className={styles.rutinListMainContainer}>
+      {rutiner.length > 0 ? (
+        rutiner.map((rutin) => (
+          <div key={rutin.id} className={styles.rutinListContainer}>
+            <div className={styles.rutiner}>
+              <div className={styles.rutinerHeader}>
+                <h3>{rutin.title}</h3>
+                <p>
+                  Prioritet: <strong>{rutin.Prioritet}</strong>
+                </p>
+              </div>
+              <div className={styles.beskrivning}>
+                Upprepas <strong>{rutin.Repetitioner}</strong> gånger.
+              </div>
+              <div className={styles.rutinFooter}>
+                <button
+                  className={styles.btn}
+                  onClick={() => updatera(rutin.id, "Öka")}
+                >
+                  +
+                </button>
+                <button
+                  className={styles.btn}
+                  onClick={() => updatera(rutin.id, "Minska")}
+                >
+                  -
+                </button>
+                <button
+                  className={styles.btn}
+                  onClick={() => updatera(rutin.id, "Nollställa")}
+                >
+                  0
+                </button>
+                <button
+                  className={styles.btn}
+                  onClick={() => tarbort(rutin.id)}
+                >
+                  Ta bort
+                </button>
+              </div>
             </div>
-            <div className={styles.listButtons}>
-              <button
-                className={styles.btn}
-                onClick={() => updatera(rutin.id, "Öka")}
-              >
-                +
-              </button>
-              <button
-                className={styles.btn}
-                onClick={() => updatera(rutin.id, "Minska")}
-              >
-                -{" "}
-              </button>
-              <button
-                className={styles.btn}
-                onClick={() => updatera(rutin.id, "Nollställa")}
-              >
-                0{" "}
-              </button>
-              <button className={styles.btn} onClick={() => tarbort(rutin.id)}>
-                {" "}
-                <i className="bi bi-trash"></i>
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
+          </div>
+        ))
+      ) : (
+        <p className={styles.ingarutin}>Inga rutiner tillagda</p>
+      )}
     </div>
   );
 }
