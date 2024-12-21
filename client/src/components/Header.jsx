600a799f9f214df48294def8d7810aeda39e0b1e
Ibrahim Osman <mr.ibrahim@hotmail.se>
2024-12-21 14:33:21 +0100

    Created Todos

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Mina Ärende</h1>
+      <p className="subtitle" style={{ color: "gray" }}>
+        Här kan du se alla dina ärenden
+      </p>
+    </div>
+  );
+};
+
+export default Header;
