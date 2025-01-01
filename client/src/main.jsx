740893f765c620b31fa66b39b7f6fc13b26757ff
Ibrahim Osman <mr.ibrahim@hotmail.se>
2025-01-01 15:14:46 +0100

    Update Deployment

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter, Route, Routes } from "react-router";
 import Sidebar from "./components/Sidebar.jsx";
-import Overview from "./pages/Overview/Overview.jsx";
+// import Overview from "./pages/Overview/Overview.jsx";
 import Todos from "./pages/Todos/Todos.jsx";
 import Habits from "./pages/Habits/Habits.jsx";
 import Events from "./pages/Events/Events.jsx";
@@ -14,6 +14,7 @@ import axios from "axios";
 import { Toaster } from "react-hot-toast";
 import { UserContextProvider } from "../context/userContext";
 import TaskDetails from "./components/Todos/Taskdetail/Taskdetail.jsx";
+import Test from "./pages/test.jsx";
 
 axios.defaults.baseURL = "http://localhost:8000";
 axios.defaults.withCredentials = true;
@@ -30,7 +31,8 @@ createRoot(document.getElementById("root")).render(
               <Route path="/" element={<App />} />
               <Route path="/signup" element={<Signup />} />
               <Route path="/signin" element={<Signin />} />
-              <Route path="/overview" element={<Overview />} />
+              {/* <Route path="/overview" element={<Overview />} /> */}
+              <Route path="/overview" element={<Test />} />
               <Route path="/todos" element={<Todos />} />
               <Route
                 path="/todos/:category/:taskIndex"
